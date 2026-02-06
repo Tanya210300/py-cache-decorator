17c12d8d4247d4ecc9fa82f14b0430a2af8ae202
Tanya <tanya210300@gmail.com>
2026-02-06 02:05:37 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from typing import Callable
 
 
-def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+def cache(func) -> Callable:
+    trash = {}
+    def inner(*args, **kwargs):
+        key = (args, tuple(kwargs.items()))
+        if key in trash:
+            print("Getting from cache")
+            return trash[key]
+        else:
+            print("Calculating new result")
+            res = func(*args, **kwargs)
+            trash[key] = res
+            return res
+    return inner
